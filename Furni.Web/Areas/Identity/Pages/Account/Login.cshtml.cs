1ce9e7dadc20197e7d4ddfe25472c0acf5496017
AbdullahGomaa <Abdullah.Goma.010170@gmail.com>
2024-06-08 22:10:34 +0300

    Fix Showing Login page after Log In

--- Furni.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Furni.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -88,8 +88,11 @@ namespace Furni.Web.Areas.Identity.Pages.Account
             public bool RememberMe { get; set; }
         }
 
-        public async Task OnGetAsync(string returnUrl = null)
+        public async Task<IActionResult> OnGetAsync(string returnUrl = null)
         {
+            if (User.Identity!.IsAuthenticated)
+                return LocalRedirect(returnUrl);
+
             if (!string.IsNullOrEmpty(ErrorMessage))
             {
                 ModelState.AddModelError(string.Empty, ErrorMessage);
@@ -103,6 +106,8 @@ namespace Furni.Web.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
 
             ReturnUrl = returnUrl;
+
+            return Page();
         }
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
