c70aab53510a839cb897038413070fb325eb26fe
Eslam ayman <106927185+Eng-Eslamayman@users.noreply.github.com>
2024-06-26 01:44:22 +0300

    - add allproduct view

---
+++ Furni.Web/Areas/Customer/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Furni.Web.Areas.Customer.Controllers
+{
+	[Area(AppRoles.Customer)]
+	public class ProductsController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+
+		public ProductsController(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+
+		public IActionResult Index()
+		{
+			var products = _unitOfWork.Products.GetAll();
+			return View();
+		}
+
+		//public IActionResult GetAll()
+		//{
+
+		//}
+	}
+}
