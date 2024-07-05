efbb15474d9c9169443800bab8912cdeac7e321f
AbdullahGomaa <Abdullah.Goma.010170@gmail.com>
2024-07-05 19:59:46 +0300

    Implementation Choose Categories Frrom Every Where at Customer Area

---
+++ Furni.Web/Areas/Customer/Components/ShopCategoriesViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Furni.Web.Areas.Customer.Components
+{
+	public class ShopCategoriesViewComponent : ViewComponent
+	{
+		private readonly IUnitOfWork _unitOfWork;
+
+		private readonly IMapper _mapper;
+		public ShopCategoriesViewComponent(IUnitOfWork unitOfWork, IMapper mapper)
+		{
+			_unitOfWork = unitOfWork;
+			_mapper = mapper;
+		}
+
+
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var categories = _unitOfWork.Categories.GetActiveCategories();
+			var viewModel = _mapper.Map<IList<CategoryHeaderViewModel>>(categories);
+			
+			return View(viewModel);
+		}
+	}
+}
